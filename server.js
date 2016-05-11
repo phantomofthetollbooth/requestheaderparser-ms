5141c2e64c244eb60f107cfe69f4cc04840dea9c
X X <X X>
2016-05-11 12:03:18 -0500

    language

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ res.sendFile(path.join(__dirname, '/public', 'index.html'));
 
 app.get("/api/whoami", function(req, res) {
 //res.sendFile(path.join(__dirname, '/public', 'index.html'));
-var languageString = req.headers.accept-language;
+var languageString = req.headers["accept-language"];
   var lang = languageString.substring(0, languageString.indexOf(','));
   var response={
-	  //language: lang,
-	  car:"dog"
+	  language: lang
   }
   res.json(response);
 });
