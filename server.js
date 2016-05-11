7980812bdecae4b08da532a23215ecdd9f1566b0
X X <X X>
2016-05-11 11:51:14 -0500

    api

--- server.js
+++ server.js
@@ -11,4 +11,15 @@ app.get("/", function(req, res) {
 res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
+app.get("/api/whoami", function(req, res) {
+//res.sendFile(path.join(__dirname, '/public', 'index.html'));
+var language = req.headers.accept-language.substring(0,accept-language.indexOf(','));
+  //var lang = langList.substring(0, langList.indexOf(','));
+  var response={
+	  language: language
+  }
+  res.json(response);
+});
+
+
 app.listen(app.get('port'));
