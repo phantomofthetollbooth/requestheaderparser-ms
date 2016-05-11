9ce573eb81e834ab56470dee2a7a0469d7dd83dd
X X <X X>
2016-05-11 12:15:38 -0500

    added ip

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-
+var requestIp = require('request-ip');
 
 var app = express();
 
@@ -12,12 +12,13 @@ res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
 app.get("/api/whoami", function(req, res) {
-//res.sendFile(path.join(__dirname, '/public', 'index.html'));
+var ip = requestIp.getClientIp(req); 
 var languageString = req.headers["accept-language"];
   var lang = languageString.substring(0, languageString.indexOf(','));
    var userAgent = req.headers["user-agent"]
   var software = userAgent.substring(userAgent.indexOf('(')+1, userAgent.indexOf(')'))
   var response={
+	  ip:ip,
 	  language: lang,
 	  software: software
   }
